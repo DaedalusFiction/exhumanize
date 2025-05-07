3729d883dc9e6a9e7263f8c4be469f4ac7c987d6
David Sorensen <daedalusfiction@gmail.com>
2025-05-07 12:41:53 -0400

    updated to work with file organization

--- components/admin/photograph/List.vue
+++ components/admin/photograph/List.vue
@@ -15,33 +15,40 @@
     </div>
     <div class="col-span-6">
       <h4 class="mb-2">Manage existing photographs</h4>
-      <div
-        v-for="photograph in photographs"
-        :key="photograph.id"
-        class="rounded transition my-1 border"
-      >
-        <div
-          @click="toggleExpand(photograph.id)"
-          class="flex p-2 justify-between rounded cursor-pointer hover:bg-backgroundAccent dark:hover:bg-backgroundAccentDarkMode"
-          :class="
-            expandedItems[photograph.id] &&
-            'bg-backgroundAccent dark:bg-backgroundAccentDarkMode'
-          "
-        >
-          <p>{{ photograph.data().name || "No Title" }}</p>
-          <p>
-            {{ new Date(photograph.data().dateUploaded).toLocaleDateString() }}
-          </p>
-        </div>
-        <div v-if="expandedItems[photograph.id]">
-          <div class="p-3">
-            <AdminPhotographForm
-              :photograph="photograph"
-              @update="handleRefreshList(photograph.id)"
-            />
+      <div class="" v-for="(category, index) in categories" :key="index">
+        <h5>{{ category.title }}</h5>
+        <div v-for="(photograph, index) in category.photographs" :key="index">
+          <div
+            v-if="photograph.data().category === category.title"
+            class="rounded transition my-1 border"
+          >
+            <div
+              @click="toggleExpand(photograph.id)"
+              class="flex p-2 justify-between rounded cursor-pointer hover:bg-backgroundAccent dark:hover:bg-backgroundAccentDarkMode"
+              :class="
+                expandedItems[photograph.id] &&
+                'bg-backgroundAccent dark:bg-backgroundAccentDarkMode'
+              "
+            >
+              <p>{{ photograph.data().alt || "No Title" }}</p>
+              <p>
+                {{
+                  new Date(photograph.data().dateUploaded).toLocaleDateString()
+                }}
+              </p>
+            </div>
+            <div v-if="expandedItems[photograph.id]">
+              <div class="p-3">
+                <AdminPhotographForm
+                  :photograph="photograph"
+                  @update="handleRefreshList(photograph.id)"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
+
       <div class="flex justify-end">
         <button class="btn" @click="handleShowMoreDocuments">More</button>
       </div>
@@ -54,8 +61,13 @@ import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
 import { db } from "~/firebase.config";
 import { ref, reactive } from "vue";
 import { AdminPhotographForm } from "#components";
+import { projects } from "~/data";
 
-const photographs = ref([]);
+const categories = reactive(
+  projects.map((project) => {
+    return { title: project.title, photographs: [] };
+  })
+);
 const expandedItems = reactive({});
 const pageSize = 10;
 const numberOfShownDocuments = ref(pageSize);
@@ -65,11 +77,20 @@ const toggleExpandNewDocument = () => {
 };
 
 const getDocuments = async (total) => {
-  const photographsRef = collection(db, "photographs");
-  const items = await getDocs(
-    query(photographsRef, limit(total), orderBy("dateUploaded", "desc"))
-  );
-  photographs.value = items.docs;
+  console.log(categories);
+  categories.forEach(async (category, index) => {
+    console.log(category.title, index);
+    const photographsRef = collection(
+      db,
+      "photographs",
+      "categories",
+      category.title
+    );
+    const items = await getDocs(
+      query(photographsRef, limit(total), orderBy("dateUploaded", "desc"))
+    );
+    categories[index].photographs = items.docs;
+  });
 };
 
 onMounted(() => {
